acd965c8add033be0ddf679533de472c30158cca
sumeetkamat1990 <choomeet@gmail.com>
2018-08-20 17:36:20 -0400

    Latest compiled Kernels for TX1 and TX2 with Jetpack 3.1

---
+++ jp3p1_tx2/imx274_60fps/lenses_2/kernel/kernel-4.4/tx2.sh
@@ -0,0 +1,4 @@
+export CROSS_COMPILE=/opt/install/bin/aarch64-unknown-linux-gnu-
+export TEGRA_KERNEL_OUT=/media/data1/jetpack3p1/64_TX2/Linux_for_Tegra_tx2/sources/kernel_out
+export ARCH=arm64
+export LOCALVERSION=-aqueti-0p3-
